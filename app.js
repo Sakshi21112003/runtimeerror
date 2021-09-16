e57805c79b3a687f1c608a0a46c7ea019f137c30
sakshi <86145621+Sakshi21112003@users.noreply.github.com>
2021-09-16 23:08:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+
+
+
+app.get('', (req, res) => {
+    res.sendFile(__diname + '/public/index.html')
+})
+
+
+
+
+
+//  Listen on port 3000
+app.listen(port, () => console.info(`Listening on port ${port}`))
